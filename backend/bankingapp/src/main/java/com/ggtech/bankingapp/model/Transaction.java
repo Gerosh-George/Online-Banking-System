d6cb18bd5065d456607e72dbd44562f867a561c1
Gerosh <geroshg@gmail.com>
2023-09-09 15:44:35 +0530

    added getter and setter for model classes

--- backend/bankingapp/src/main/java/com/ggtech/bankingapp/model/Transaction.java
+++ backend/bankingapp/src/main/java/com/ggtech/bankingapp/model/Transaction.java
@@ -6,10 +6,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.Data;
+
 
 @Entity
-@Data
 public class Transaction {
 
 	@Id
@@ -26,5 +25,71 @@ public class Transaction {
 	@ManyToOne
 	@JoinColumn(name="accno")
 	private Account acc_no;
+
+	public long getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getTransType() {
+		return transType;
+	}
+
+	public void setTransType(String transType) {
+		this.transType = transType;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public long getAccFrom() {
+		return accFrom;
+	}
+
+	public void setAccFrom(long accFrom) {
+		this.accFrom = accFrom;
+	}
+
+	public long getAccTo() {
+		return accTo;
+	}
+
+	public void setAccTo(long accTo) {
+		this.accTo = accTo;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Account getAcc_no() {
+		return acc_no;
+	}
+
+	public void setAcc_no(Account acc_no) {
+		this.acc_no = acc_no;
+	}
+	
+	
 	
 }

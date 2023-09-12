727cd6696cc922840235bac21e24d75d872648ef
shreydevep <shrey.devep@gmail.com>
2023-09-12 20:55:47 +0530

    Added AccountPage

--- frontend/bankapp/src/Components/AccountPage.jsx
+++ frontend/bankapp/src/Components/AccountPage.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@mui/material";
+import InputComponent from "./InputComponent";
 
-function AccountPage() {
+const AccountPage = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "100%",
+    width: 350,
+    margin: "40px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
+
+  const baseURL = "http://localhost:8080/hello";
+  const navigate = useNavigate();
+
+  
+  const [accountType, setAccountType] = useState("");
+  const [openingDate, setOpeningDate] = useState("");
+  const [ifsc, setIfsc] = useState("");
+  const [branch, setBranch] = useState("");
+  
+
+  
+  const accountTypeHandler = (event) => {
+    setAccountType(event.target.value);
+  };
+
+  const openingDateHandler = (event) => {
+    setOpeningDate(event.target.value);
+  };
+
+  const ifscHandler = (event) => {
+    setIfsc(event.target.value);
+  };
+
+  const branchHandler = (event) => {
+    setBranch(event.target.value);
+  };
+
+
+  const submitActionHandler = (event) => {
+    event.preventDefault();
+    console.log(event);
+    axios
+      .post(baseURL, {
+        accountType,
+        ifsc,
+        openingDate,
+        branch
+      })
+      .then((response) => {
+        console.log(response);
+        alert(response.data.message);
+
+        //navigate("/account");
+      })
+      .catch((error) => {
+        alert("error===" + error);
+      });
+  };
+
   return (
-    <div>AccountPage</div>
-  )
-}
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid item align="center">
+          <Avatar style={avatarStyle}></Avatar>
+          <h2>Create Account</h2>
+        </Grid>
+        <Grid item xs={12}>
+          <InputComponent
+            _id={"Account Type"}
+            _value={accountType}
+            _placeholder={"Account Type"}
+            _changeHandler={accountTypeHandler}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <InputComponent
+            _id={"IFSC"}
+            _value={ifsc}
+            _placeholder={"IFSC"}
+            _changeHandler={ifscHandler}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <InputComponent
+            _id={"Branch"}
+            _value={branch}
+            _placeholder={"Brancg"}
+            _changeHandler={branchHandler}
+          />
+        </Grid>
+       
 
-export default AccountPage
+     
+       
+        <Grid item xs={12}>
+          <InputComponent
+            _id={"Account Opening Date"}
+            _value={openingDate}
+            _placeholder={"Opening Date"}
+            _changeHandler={openingDateHandler}
+          />
+        </Grid>
+
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+          onClick={submitActionHandler}
+        >
+          Open Account
+        </Button>
+      </Paper>
+    </Grid>
+  );
+};
+export default AccountPage;
